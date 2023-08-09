cdc99af25743ab5c615030938ff1834f98c1e931
versatile153 <ceze46321@gmail>
2023-08-09 10:08:14 +0100

    first commit

---
+++ .vscode/js/index.js
@@ -0,0 +1,11 @@
+// $(document).ready(function () {
+//     // Initialize the carousel
+//     $('#myCarousel').carousel({
+//         interval: 2000 // Change the slide interval here (in milliseconds)
+//     });
+// });
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     // Hide the preloader
+//     document.getElementById("preloader").style.display = "none";
+// });
