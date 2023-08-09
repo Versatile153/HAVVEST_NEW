0140acbe45a139da8f613f30e01eace6036c3bfa
versatile153 <ceze46321@gmail>
2023-08-09 11:49:04 +0100

    third commit

--- js/index.js
+++ js/index.js
@@ -2,4 +2,23 @@
 setTimeout(function() {
     document.querySelector('.loading-overlay').style.display = 'none';
   }, 3000); // Hide the loading indicator after 3 seconds (adjust as needed)
-  
+
+// Show the scroll-to-top button when user scrolls down
+window.addEventListener("scroll", showScrollButton);
+
+function showScrollButton() {
+  const scrollButton = document.getElementById("scrollButton");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollButton.style.display = "block";
+  } else {
+    scrollButton.style.display = "none";
+  }
+}
+
+// Scroll to the top when the button is clicked
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth" // Enable smooth scrolling
+  });
+}
