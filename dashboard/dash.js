5dc4eb9a2287c6df7485b340a587343d33a0ae8b
versatile153 <ceze46321@gmail.com>
2023-08-19 04:14:03 -0700

    language selector added

--- dashboard/dash.js
+++ dashboard/dash.js
@@ -76,3 +76,51 @@ const allSideMenu = document.querySelectorAll('#sidebar .side-menu.top li a');
                 document.body.classList.remove('dark');
             }
         })
+
+        
+        $(document).ready(function () {
+            // Show modal and set action link when action link is clicked
+            $('.action-link').click(function (e) {
+                e.preventDefault(); // Prevent default link behavior
+                const action = $(this).data('action');
+                const href = $(this).attr('href');
+                $('#confirmationModal').data('action-link', href).modal('show');
+            });
+    
+            // Handle confirmation action
+            $('#confirmAction').click(function () {
+                const actionLink = $('#confirmationModal').data('action-link');
+                if (actionLink) {
+                    // Hide the modal
+                    $('#confirmationModal').modal('hide');
+                    // Navigate to the specified address
+                    window.location.href = actionLink;
+                }
+            });
+        });
+
+        // Update the progress bar width as the task progresses
+function updateProgressBar(progress) {
+    const progressBar = document.getElementById('progressBar');
+    progressBar.style.width = progress + '%';
+
+    // Update body brightness based on progress
+    const brightness = progress;
+    document.body.style.filter = `brightness(${brightness}%)`;
+
+    // Hide the progress bar when brightness is at maximum
+    if (brightness >= 100) {
+        progressBar.style.display = 'none';
+    }
+}
+
+// Simulate progress
+let progress = 0;
+const interval = setInterval(() => {
+    progress += 10;
+    if (progress <= 100) {
+        updateProgressBar(progress);
+    } else {
+        clearInterval(interval);
+    }
+}, 1000);
